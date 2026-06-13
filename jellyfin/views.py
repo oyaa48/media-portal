ac4dee02a8ff706b6dfc3a1d3e7cd871dc46f2ac
Oya Gecco <me@xoyaa.com>
2026-06-13 23:35:46 +0200

    Add library browsing

--- jellyfin/views.py
+++ jellyfin/views.py
@@ -28,3 +28,33 @@ def index(request):
             "libraries": data["Items"],
         },
     )
+
+
+def library(request, library_id):
+    url = os.getenv("JELLYFIN_URL")
+    api_key = os.getenv("JELLYFIN_API_KEY")
+
+    if api_key is None:
+        raise ValueError("JELLYFIN_API_KEY is not set")
+
+    response = requests.get(
+        f"{url}/Items",
+        params={
+            "ParentId": library_id,
+        },
+        headers={"X-Emby-Token": api_key},
+        timeout=10,
+    )
+
+    data = response.json()
+
+    library_name = request.GET.get("name", "Library")
+
+    return render(
+        request,
+        "jellyfin/library.html",
+        {
+            "library_name": library_name,
+            "items": data["Items"],
+        },
+    )
